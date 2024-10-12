86c619ffd13e4810d75d2f7a1f6897b34685ad7b
Elena Hayrova <hayrova.el@icloud.com>
2024-10-12 21:28:26 +0300

    task 1

---
+++ 1/Navigation1/Profile/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  Navigation1
+//
+//  Created by Елена Хайрова on 12.10.2024.
+//
+
+import UIKit
+
+class User {
+    let login: String
+    let fullName: String
+    let avatar: UIImage
+    let status: String
+
+    init(login: String, fullName: String, avatar: UIImage, status: String) {
+        self.login = login
+        self.fullName = fullName
+        self.avatar = avatar
+        self.status = status
+    }
+}
+protocol UserService {
+    func getUser (byLogin login: String) -> User?
+}
+
+class CurrentUserService: UserService {
+    private let user: User
+
+    init(user: User) {
+        self.user = user
+    }
+
+    func getUser (byLogin login: String) -> User? {
+        return login == user.login ? user : nil
+    }
+}
+

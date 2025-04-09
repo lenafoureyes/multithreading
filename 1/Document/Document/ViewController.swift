41fa63dad44aeff7b1e0b73e37cf0c3cd9226958
Elena Hayrova <hayrova.el@icloud.com>
2025-04-09 19:15:17 +0300

    1

---
+++ 1/Document/Document/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Document
+//
+//  Created by Елена Хайрова on 09.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

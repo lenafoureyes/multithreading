d332c0cb64b77012c6fa5457631baf52afeff6ea
Elena Hayrova <hayrova.el@icloud.com>
2025-05-16 21:36:57 +0300

    исправление 1

--- 1/MapDemoApp/MapDemoApp/MapViewController.swift
+++ 1/MapDemoApp/MapDemoApp/MapViewController.swift
@@ -14,43 +14,41 @@ class MapViewController: UIViewController {
     // MARK: - UI Elements
     private let mapView = MKMapView()
     private let routeButton = UIButton(type: .system)
+    private let clearButton = UIButton(type: .system)
     
     // MARK: - Properties
     private let locationManager = CLLocationManager()
     private var currentLocation: CLLocation?
     private var destinationCoordinate: CLLocationCoordinate2D?
+    private var destinationAnnotation: MKPointAnnotation?
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        #if targetEnvironment(simulator)
-        mapView.isPitchEnabled = false
-        mapView.isRotateEnabled = false
-        #endif
-        
         setupViews()
         setupConstraints()
         configureMap()
         setupLocationManager()
-        addTestPin()
+        setupGestureRecognizers()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         view.bringSubviewToFront(routeButton)
+        view.bringSubviewToFront(clearButton)
     }
     
     // MARK: - Setup Methods
     private func setupViews() {
         view.backgroundColor = .white
-        title = "Карта с маршрутом"
+        title = "Навигатор"
         
         // Настройка карты
         view.addSubview(mapView)
         mapView.translatesAutoresizingMaskIntoConstraints = false
         
-        // Настройка кнопки
+        // Настройка кнопки маршрута
         routeButton.setTitle("Построить маршрут", for: .normal)
         routeButton.backgroundColor = .systemBlue
         routeButton.setTitleColor(.white, for: .normal)
@@ -60,9 +58,24 @@ class MapViewController: UIViewController {
         routeButton.layer.shadowRadius = 4
         routeButton.layer.shadowOpacity = 0.3
         routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        routeButton.isHidden = true
+        
+        // Настройка кнопки очистки
+        clearButton.setTitle("Очистить", for: .normal)
+        clearButton.backgroundColor = .systemRed
+        clearButton.setTitleColor(.white, for: .normal)
+        clearButton.layer.cornerRadius = 8
+        clearButton.layer.shadowColor = UIColor.black.cgColor
+        clearButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        clearButton.layer.shadowRadius = 4
+        clearButton.layer.shadowOpacity = 0.3
+        clearButton.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+        clearButton.isHidden = true
         
         view.addSubview(routeButton)
+        view.addSubview(clearButton)
         routeButton.translatesAutoresizingMaskIntoConstraints = false
+        clearButton.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setupConstraints() {
@@ -73,22 +86,20 @@ class MapViewController: UIViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
             routeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            routeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            routeButton.widthAnchor.constraint(equalToConstant: 200),
-            routeButton.heightAnchor.constraint(equalToConstant: 50)
+            routeButton.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -10),
+            routeButton.widthAnchor.constraint(equalToConstant: 180),
+            routeButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            clearButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            clearButton.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 10),
+            clearButton.widthAnchor.constraint(equalToConstant: 180),
+            clearButton.heightAnchor.constraint(equalToConstant: 50)
         ])
-        
-        view.bringSubviewToFront(routeButton)
     }
     
     private func configureMap() {
         mapView.delegate = self
         mapView.showsUserLocation = true
-        
-        #if targetEnvironment(simulator)
-        mapView.showsBuildings = false
-        #endif
-        
         mapView.mapType = .mutedStandard
         mapView.pointOfInterestFilter = .excludingAll
         mapView.showsTraffic = false
@@ -96,7 +107,7 @@ class MapViewController: UIViewController {
         mapView.showsScale = true
         
         mapView.cameraZoomRange = MKMapView.CameraZoomRange(
-            minCenterCoordinateDistance: 1000,
+            minCenterCoordinateDistance: 100,
             maxCenterCoordinateDistance: 20000
         )
         
@@ -121,41 +132,67 @@ class MapViewController: UIViewController {
         }
     }
     
-    private func addTestPin() {
-        let coordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-        let annotation = MKPointAnnotation()
-        annotation.title = "Пункт назначения"
-        annotation.coordinate = coordinate
-        mapView.addAnnotation(annotation)
+    private func setupGestureRecognizers() {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        longPressRecognizer.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longPressRecognizer)
+    }
+    
+    // MARK: - Action Methods
+    @objc private func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            
+            // Удаляем предыдущую точку назначения
+            if let previousAnnotation = destinationAnnotation {
+                mapView.removeAnnotation(previousAnnotation)
+            }
+            
+            // Добавляем новую точку назначения
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "Пункт назначения"
+            mapView.addAnnotation(annotation)
+            
+            destinationCoordinate = coordinate
+            destinationAnnotation = annotation
+            
+            routeButton.isHidden = false
+            clearButton.isHidden = false
+        }
+    }
+    
+    @objc private func routeButtonTapped() {
+        guard let destination = destinationCoordinate else {
+            showAlert(message: "Сначала выберите точку назначения")
+            return
+        }
         
-        destinationCoordinate = coordinate
+        #if targetEnvironment(simulator)
+        let startCoordinate = mapView.userLocation.coordinate
+        #else
+        guard let startCoordinate = currentLocation?.coordinate else {
+            showAlert(message: "Не удалось определить ваше местоположение")
+            return
+        }
+        #endif
+        
+        drawRoute(from: startCoordinate, to: destination)
+    }
+    
+    @objc private func clearButtonTapped() {
+        mapView.removeOverlays(mapView.overlays)
+        if let annotation = destinationAnnotation {
+            mapView.removeAnnotation(annotation)
+        }
+        destinationCoordinate = nil
+        destinationAnnotation = nil
+        routeButton.isHidden = true
+        clearButton.isHidden = true
     }
     
     // MARK: - Route Methods
-    @objc private func routeButtonTapped() {
-        #if targetEnvironment(simulator)
-        let startCoordinate = CLLocationCoordinate2D(latitude: 55.7909, longitude: 37.5867)
-        let endCoordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-        
-        let distance = CLLocation(latitude: startCoordinate.latitude, longitude: startCoordinate.longitude)
-            .distance(from: CLLocation(latitude: endCoordinate.latitude, longitude: endCoordinate.longitude))
-        
-        guard distance > 500 else {
-            showAlert(message: "Точки слишком близко для построения маршрута")
-            return
-        }
-        
-        drawRoute(from: startCoordinate, to: endCoordinate)
-        #else
-        guard let startCoordinate = currentLocation?.coordinate,
-              let endCoordinate = destinationCoordinate else {
-            showAlert(message: "Не удалось определить местоположение")
-            return
-        }
-        drawRoute(from: startCoordinate, to: endCoordinate)
-        #endif
-    }
-    
     private func drawRoute(from start: CLLocationCoordinate2D, to end: CLLocationCoordinate2D) {
         mapView.removeOverlays(mapView.overlays)
         
@@ -179,26 +216,17 @@ class MapViewController: UIViewController {
             }
             
             self.mapView.addOverlay(route.polyline)
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                let padding = UIEdgeInsets(top: 100, left: 50, bottom: 150, right: 50)
-                let mapRect = route.polyline.boundingMapRect
-                let fittedRect = self.mapView.mapRectThatFits(mapRect, edgePadding: padding)
-                
-                UIView.animate(withDuration: 2.0, delay: 0, options: [.curveEaseInOut]) {
-                    let newRegion = MKCoordinateRegion(fittedRect)
-                    let fixedAltitude = max(newRegion.span.latitudeDelta, newRegion.span.longitudeDelta) * 1.5
-                    
-                    let camera = MKMapCamera(
-                        lookingAtCenter: newRegion.center,
-                        fromDistance: fixedAltitude * 111_320,
-                        pitch: 0,
-                        heading: 0
-                    )
-                    
-                    self.mapView.setCamera(camera, animated: true)
-                }
-            }
+            self.zoomToRoute(route: route)
+        }
+    }
+    
+    private func zoomToRoute(route: MKRoute) {
+        let padding = UIEdgeInsets(top: 100, left: 50, bottom: 150, right: 50)
+        let mapRect = route.polyline.boundingMapRect
+        let fittedRect = mapView.mapRectThatFits(mapRect, edgePadding: padding)
+        
+        UIView.animate(withDuration: 1.0) {
+            self.mapView.setVisibleMapRect(fittedRect, edgePadding: padding, animated: true)
         }
     }
     
@@ -215,16 +243,16 @@ extension MapViewController: CLLocationManagerDelegate {
         guard let location = locations.last else { return }
         currentLocation = location
         
-        let region = MKCoordinateRegion(center: location.coordinate,
-                                      latitudinalMeters: 1000,
-                                      longitudinalMeters: 1000)
-        mapView.setRegion(region, animated: true)
+        if locations.count == 1 {
+            let region = MKCoordinateRegion(center: location.coordinate,
+                                          latitudinalMeters: 1000,
+                                          longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        #if targetEnvironment(simulator)
-        print("Игнорируем ошибку локации в симуляторе")
-        #else
+        #if !targetEnvironment(simulator)
         showAlert(message: "Ошибка получения локации: \(error.localizedDescription)")
         #endif
     }
@@ -249,7 +277,7 @@ extension MapViewController: MKMapViewDelegate {
         if overlay is MKPolyline {
             let renderer = MKPolylineRenderer(overlay: overlay)
             renderer.strokeColor = UIColor.systemBlue.withAlphaComponent(0.7)
-            renderer.lineWidth = 6
+            renderer.lineWidth = 5
             renderer.lineDashPattern = [2, 6]
             return renderer
         }
@@ -259,22 +287,19 @@ extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard !(annotation is MKUserLocation) else { return nil }
         
-        let identifier = "pin"
+        let identifier = "destinationPin"
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
         
         if annotationView == nil {
             annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView?.glyphImage = UIImage(systemName: "flag.fill")
+            annotationView?.markerTintColor = .systemRed
         } else {
             annotationView?.annotation = annotation
         }
         
         return annotationView
-    }
-    
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        routeButtonTapped()
     }
     
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
